8c0bc2036836d927befa5259630df7b7c1fb852a
aizimpamvu <hallyiizza200@gmail.com>
2022-09-29 01:19:52 +0200

    scrap movies

--- main.py
+++ main.py
@@ -4,5 +4,19 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(URL)
 
+# movie web page
+
+mv_web_page =response.text
+
+soup = BeautifulSoup(mv_web_page, "html.parser")
+
+movies_list = []
+all_movies = soup.find_all(name="h3", class_="title")
+
+for movie in all_movies:
+    # print(movie.getText())
+    with open("movies.txt", "w", encoding="utf8") as file:
+        file.write(movie.getText())
 
